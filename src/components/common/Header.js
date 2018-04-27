9da639ba43bebff6503e2e0856b84be620725611
SEAjamieD <JamieDegnan@zumiez.com>
2018-04-27 08:27:14 -0700

    header patch

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,8 +3,11 @@ import './header.css';
 
 const Header = () => {
   return (
-      <div className="header__container full-flex">
-        <h1 className="header__title">trailer<span>pass</span></h1>
+      <div>
+        <div className="pwa-header-patch"></div>
+        <div className="header__container full-flex">
+          <h1 className="header__title">trailer<span>pass</span></h1>
+        </div>
       </div>
   );
 }
